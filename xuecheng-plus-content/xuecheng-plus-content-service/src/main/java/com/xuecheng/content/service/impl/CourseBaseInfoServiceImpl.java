1acf61c9df3797ef26bd5910b3ba77c39d26748d
jtf <2024649355@qq.com>
2024-01-21 00:14:50 +0800

    content模块的课程信息管理分页查询

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
@@ -0,0 +1,69 @@
+package com.xuecheng.content.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.mapper.CourseCategoryMapper;
+import com.xuecheng.content.mapper.CourseMarketMapper;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.model.po.CourseMarket;
+import com.xuecheng.content.service.CourseBaseInfoService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resources;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description TODO
+ * @date 2023/2/12 10:16
+ */
+@Slf4j
+@Service
+public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
+    @Autowired
+    CourseBaseMapper courseBaseMapper;
+
+
+
+    @Override
+    public PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto courseParamsDto) {
+
+        //拼装查询条件
+        LambdaQueryWrapper<CourseBase> queryWrapper = new LambdaQueryWrapper<>();
+        //根据名称模糊查询,在sql中拼接 course_base.name like '%值%'
+        queryWrapper.like(StringUtils.isNotEmpty(courseParamsDto.getCourseName()),CourseBase::getName,courseParamsDto.getCourseName());
+        //根据课程审核状态查询 course_base.audit_status = ?
+        queryWrapper.eq(StringUtils.isNotEmpty(courseParamsDto.getAuditStatus()), CourseBase::getAuditStatus,courseParamsDto.getAuditStatus());
+        //todo:按课程发布状态查询
+        queryWrapper.eq(StringUtils.isNotEmpty(courseParamsDto.getPublishStatus()),CourseBase::getStatus,courseParamsDto.getPublishStatus());
+
+        //创建page分页参数对象，参数：当前页码，每页记录数
+        Page<CourseBase> page = new Page<>(pageParams.getPageNo(), pageParams.getPageSize());
+        //开始进行分页查询
+        Page<CourseBase> pageResult = courseBaseMapper.selectPage(page, queryWrapper);
+        //数据列表
+        List<CourseBase> items = pageResult.getRecords();
+        //总记录数
+        long total = pageResult.getTotal();
+
+        //List<T> items, long counts, long page, long pageSize
+        PageResult<CourseBase> courseBasePageResult = new PageResult<CourseBase>(items,total,pageParams.getPageNo(), pageParams.getPageSize());
+        return  courseBasePageResult;
+    }
+
+
+
+
+
+}
