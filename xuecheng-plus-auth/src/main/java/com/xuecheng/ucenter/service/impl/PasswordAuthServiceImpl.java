9dd203a50e02902000d9a3de574009a8c1ad287d
jtf <2024649355@qq.com>
2024-03-03 02:12:27 +0800

    统一认证入口3：完成账号密码登录，根据认证类型匹配到账号密码登录的实现类，然后完成具体操作，最后将数据封装在json中返回

--- xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/PasswordAuthServiceImpl.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/PasswordAuthServiceImpl.java
@@ -27,36 +27,18 @@ public class PasswordAuthServiceImpl implements AuthService {
  @Autowired
  PasswordEncoder passwordEncoder;
 
- @Autowired
- CheckCodeClient checkCodeClient;
 
  @Override
  public XcUserExt execute(AuthParamsDto authParamsDto) {
   //账号
   String username = authParamsDto.getUsername();
 
-  //输入的验证码
-  String checkcode = authParamsDto.getCheckcode();
-  //验证码对应的key
-  String checkcodekey = authParamsDto.getCheckcodekey();
-
-  if(StringUtils.isEmpty(checkcode) || StringUtils.isEmpty(checkcodekey)){
-   throw new RuntimeException("请输入的验证码");
-  }
-
-  //远程调用验证码服务接口去校验验证码
-  Boolean verify = checkCodeClient.verify(checkcodekey, checkcode);
-  if(verify == null||!verify){
-   throw new RuntimeException("验证码输入错误");
-  }
-
-
   //账号是否存在
   //根据username账号查询数据库
   XcUser xcUser = xcUserMapper.selectOne(new LambdaQueryWrapper<XcUser>().eq(XcUser::getUsername, username));
 
   //查询到用户不存在，要返回null即可，spring security框架抛出异常用户不存在
-  if(xcUser==null){
+  if (xcUser == null) {
    throw new RuntimeException("账号不存在");
   }
 
@@ -65,13 +47,13 @@ public class PasswordAuthServiceImpl implements AuthService {
   String passwordDb = xcUser.getPassword();
   //拿 到用户输入的密码
   String passwordForm = authParamsDto.getPassword();
-  //校验密码
+  //校验密码，因为在DaoAuthenticationProviderCustom类中重写了校验密码的方式，框架不会自动校验密码了，所以要手动校验
   boolean matches = passwordEncoder.matches(passwordForm, passwordDb);
-  if(!matches){
+  if (!matches) {
    throw new RuntimeException("账号或密码错误");
   }
   XcUserExt xcUserExt = new XcUserExt();
-  BeanUtils.copyProperties(xcUser,xcUserExt);
+  BeanUtils.copyProperties(xcUser, xcUserExt);
 
   return xcUserExt;
  }
--- xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/UserServiceImpl.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/UserServiceImpl.java
@@ -31,44 +31,45 @@ public class UserServiceImpl implements UserDetailsService {
         //将传入的json转成AuthParamsDto对象
         AuthParamsDto authParamsDto = null;
         try {
-            authParamsDto = JSON.parseObject(s,AuthParamsDto.class);
-        }catch (Exception e){
+            authParamsDto = JSON.parseObject(s, AuthParamsDto.class);
+        } catch (Exception e) {
             throw new RuntimeException("请求认证参数不符合要求");
         }
 
         //根据认证类型获取指定的认证方式（从容器中拿到）,通过名字拿到，因为在认证方式的实现类中的@Service上写了名字
         String authType = authParamsDto.getAuthType();
-        String beanName = authType+"_authservice";
+        String beanName = authType + "_authservice";
         //从Spring的ApplicationContext中获取一个名为"beanName"的Bean，并将其转换为AuthService类型的对象
-        AuthService authService = applicationContext.getBean(beanName, AuthService.class);
+        AuthService authService = applicationContext.getBean(beanName, AuthService.class);//多态，向上转型
 
         //调用统一的execute方法完成认证
         XcUserExt execute = authService.execute(authParamsDto);
 
+        UserDetails userPrincipal = getUserPrincipal(execute);
 
-        //账号
-        String username = authParamsDto.getUsername();
+        return userPrincipal;
 
-        //数据库中查用户是否存在
-        XcUser user = xcUserMapper.selectOne(new LambdaQueryWrapper<XcUser>().eq(XcUser::getUsername, username));
-        if(user==null){
-            //返回空表示用户不存在，框架收到空会报用户不存在异常
-            return null;
-        }
-        //取出数据库存储的正确密码，传给框架，框架比对输入的密码对不对
-        String password  =user.getPassword();
-        //用户权限,如果不加报Cannot pass a null GrantedAuthority collection
-        String[] authorities= {"test"};
-        //创建UserDetails对象,权限信息待实现授权功能时再向UserDetail中加入
-
-        //密码为安全信息，所以不防止jwt中
-        user.setPassword(null);
-        //转成json，然后传递给jwt
-        String userJson = JSON.toJSONString(user);
-
-        //比对用户是否存在已经在前面做了，所以下面这条代码只需要比对密码信息和传递用户信息
-        UserDetails userDetails = User.withUsername(userJson).password(password).authorities(authorities).build();
-
-        return userDetails;
     }
-}
+        /**
+         * @description 查询用户信息，封装到令牌中
+         * @param user  用户id，主键
+         * @return com.xuecheng.ucenter.model.po.XcUser 用户信息
+         * @author Mr.M
+         * @date 2022/9/29 12:19
+         */
+        public UserDetails getUserPrincipal(XcUserExt user){
+            //用户权限,如果不加报Cannot pass a null GrantedAuthority collection
+            String[] authorities = {"p1"};
+            String password = user.getPassword();
+            //为了安全在令牌中不放密码
+            user.setPassword(null);
+            //将user对象转json
+            String userString = JSON.toJSONString(user);
+            //创建UserDetails对象
+            UserDetails userDetails = User.withUsername(userString).password(password ).authorities(authorities).build();
+            return userDetails;
+        }
+
+
+    }
+
